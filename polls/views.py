072e6bccbd72e47d1dd1cc26b9fe1358a0f583af
honuKdestine <honudestine933@gmail.com>
2025-10-31 15:49:02 +0000

    Modify index.html to show available questions and choices

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+
 
 # Create your views here.
+def index(request):
+    latest_questions = Question.objects.all()
+    output = ",".join([q.question_text for q in latest_questions])
+    # return render(request, "polls/index.html", {"latest_questions": latest_questions})
+    return render(
+        request, "polls/index.html", {"latest_question_list": latest_questions}
+    )
